dacfae76672d6003fe6f60618331fee4f5bb1a0f
SAIGUSA Tomotada <smmy@nectarition.jp>
2023-04-21 18:14:29 +0900

    config追加

---
+++ src/content/config.ts
@@ -0,0 +1,24 @@
+import { z, defineCollection } from 'astro:content'
+
+const pageCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    showMenu: z.boolean().optional(),
+    menuIcon: z.string().optional(),
+    menuOrder: z.number().optional()
+  })
+})
+
+const postCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    createdAt: z.date(),
+    updatedAt: z.date().optional(),
+    authorName: z.string().default('AN-P')
+  })
+})
+
+export const collections = {
+  'pages': pageCollection,
+  'posts': postCollection
+}
--- src/content/posts/20230421-helloworld.md
+++ src/content/posts/20230421-helloworld.md
@@ -1,6 +1,8 @@
 ---
-title: 'テスト'
-slug: 'helloworld-test'
+title: z.string(),
+slug: helloworld-test
+createdAt: 2023-04-21T00:00:00Z
+updatedAt: 2023-04-21T00:00:00Z
 ---
 
 テスト
